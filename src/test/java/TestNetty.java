5e3b37670883b98dd3769ae7552e4376bee98d59
WarmCongee <1979557180@qq.com>
2022-04-05 15:26:57 +0800

    add junit test function

--- src/test/java/TestNetty.java
+++ src/test/java/TestNetty.java
@@ -1,19 +1,28 @@
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class TestNetty {
+
+    public static final Logger logger = LoggerFactory.getLogger("InfoLogger");
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-
+        logger.info("Junit test start");
     }
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         Assert.assertTrue(true);
     }
 
+
+
+    @AfterClass
+    public static void endJunitTest() throws Exception {
+        logger.info("Junit test start");
+    }
 
 
 }
